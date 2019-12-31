9d2ad381d1e4f09311d6c8ea3ab5fc73edc9ad48
tupham9506 <tupham9506@gmail.com>
2019-12-31 09:10:19 +0700

    add main js

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <com-example />
+  </div>
+</template>
+
+<script>
+import comExample from './components/com-example'
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      fromApp: ''
+    }
+  },
+  components: {
+    comExample
+  }
+}
+</script>
+
+<style>
+
+</style>
